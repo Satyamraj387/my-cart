38ceb903ab2f4b61e0ce1c3ce32233d92ced9da9
Satyam_Raj <satyamraj387@gmail.com>
2022-02-16 20:49:06 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -53,11 +53,6 @@ componentDidMount(){
 
 //onsnapshot is called every time something changes in database
 .onSnapshot((snapshot)=>{
-  console.log(snapshot);
-
-  snapshot.docs.map((doc)=>{
-    return doc.data;
-});
 const products = snapshot.docs.map((doc)=>{
   const data = doc.data();
   data['id'] = doc.id;
@@ -70,19 +65,61 @@ this.setState({
 })
 }
 handleIncreaseQuantity = (product)=>{
-const {products} = this.state;
-const index = products.indexOf(product);
-products[index].qty +=1;
-this.setState({
-    products: products
+// const {products} = this.state;
+// const index = products.indexOf(product);
+// products[index].qty +=1;
+// this.setState({
+//     products: products
+// })
+const docRef = db.collection('products').doc(product.id);
+docRef
+.update({
+  qty: product.qty +1
+})
+.then(()=>{
+  console.log('updated successfully')
+})
+.catch((err)=>{
+  console.log('error in updating qty', err);
 })
 }
+handleDecreaseQuantity = (product)=>{
+  // const {products} = this.state;
+  // const index = products.indexOf(product);
+  // products[index].qty +=1;
+  // this.setState({
+  //     products: products
+  // })
+  const docRef = db.collection('products').doc(product.id);
+  if(product.qty===0){
+    return;
+  }
+  docRef
+  .update({
+    qty: product.qty -1
+  })
+  .then(()=>{
+    console.log('updated successfully')
+  })
+  .catch((err)=>{
+    console.log('error in updating qty', err);
+  })
+}
 handleDeleteProduct = (id)=>{
-    const {products} = this.state;
-    const items = products.filter((item)=> item.id!==id);
-    this.setState({
-        products:items
-    });
+    // const {products} = this.state;
+    // const items = products.filter((item)=> item.id!==id);
+    // this.setState({
+    //     products:items
+    // });
+    const docRef = db.collection('products').doc(id);
+    docRef
+    .delete()
+    .then(()=>{
+      console.log('deleted successfully')
+    })
+    .catch((err)=>{
+      console.log('error in deleting qty', err);
+    })
 }
 getCartCount = ()=>{
   const{products}= this.state;
@@ -100,6 +137,22 @@ getTotal = ()=>{
   });
   return total;
 }
+// addProduct = ()=>{
+//   db
+//   .collection('products')
+//   .add({
+//     img: '',
+//     price: 10000,
+//     qty:1,
+//     title: 'AirPods'
+//   })
+//   .then((docRef)=>{
+//     console.log('product has been added',docRef)
+//   })
+//   .catch((err)=>{
+//     console.log('error in adding producr', err);
+//   })
+// }
 
   render(){
     const { products, loading } = this.state;
@@ -109,6 +162,7 @@ getTotal = ()=>{
      <Navbar
      count={this.getCartCount()} 
      />
+     {/* <button onClick={this.addProduct}>Add a product</button> */}
      <Cart 
      products={products}
      increase={this.handleIncreaseQuantity}
